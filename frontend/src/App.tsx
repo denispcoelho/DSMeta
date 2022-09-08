b53099385a869716753eebc654a53baf52412c10
Denis Coelho <denispcoelho@gmail.com>
2022-09-08 20:20:10 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
-
+import NotificationButton from "./componentes"
 
 function App() {
   return(
-    <h1>Olá Mundo</h1>
+    <>
+      <h1>Olá Mundo</h1>
+      <NotificationButton/>
+      <NotificationButton/>
+    </>
   )
 }
 
