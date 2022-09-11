1dc9cc27ea2dffb3c3ae5a43cbc5d8d5ffe63eb0
denispcoelho <denispcoelho@gmail.com>
2022-09-11 18:49:56 -0300

    send notification

--- frontend/src/componentes/NotificationButton/index.tsx
+++ frontend/src/componentes/NotificationButton/index.tsx
@@ -1,9 +1,23 @@
+import axios, { Axios } from "axios";
 import icon from "../../assets/img/Notificationicon.svg"
+import { BASE_URL } from "../../utils/request";
 import '../NotificationButton/styles.css'
 
-function NotificationButton() {
+type Props = {
+    saleId: number;
+
+}
+
+function handleClick(id: number){
+    axios(`${BASE_URL}/sales/${id}/notification`)
+    .then(response => {
+        console.log("SUCESSO");
+    });
+}
+
+function NotificationButton({saleId}: Props) {
     return(
-        <div className="redbtm">
+        <div className="redbtm" onClick={()=> handleClick(saleId)}>
             <img src={icon} alt="notificar"/>
         </div>
     )
